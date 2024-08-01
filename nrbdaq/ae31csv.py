ea97e61bc6e96672d9275b1415915e4713bdae4c
jkl <joerg.klausen@meteoswiss.ch>
2024-08-01 15:26:02 +0000

    use schedule and context managers

--- nrbdaq/ae31csv.py
+++ nrbdaq/ae31csv.py
@@ -2,29 +2,40 @@
 Basic function to open serial port, receive data every 5', append to CSV file, 
 whereby a new CSV file called 'AE31_yyyymmdd.dat' is generated every day.
 """
+import os
+import schedule
 import serial
+import time
 from datetime import datetime
-# open serial port
-ser = serial.Serial('/dev/ttyUSB0', 9600, 8, 'N', 1, timeout=360)
 
-# open file for the day and get ready to write to it
-dte_today = datetime.now().strftime('%Y%m%d')
-file_data = open(f"data/ae31/AE31_{dte_today}.csv", 'w')
-print(f"# Reading data and writing to AE31_{dte_today}.csv")
-data_received = " "
-
-# Listen for the input, exit if nothing received in timeout period
-while True:
-    while data_received:
+def retrieve_and_save_data(serial_port: str, data_path: str):
+    # open serial port
+    with serial.Serial(serial_port, 9600, 8, 'N', 1, timeout=270) as ser:
         data_received = ser.readline().decode()
-        print(f"{data_received[:80]} ..."),
-        
         dtm = datetime.now().isoformat()
-        if dte_today==datetime.now().strftime('%Y%m%d'):
-            file_data.write(f"{dtm}, {data_received}")
+        print(f"{dtm}: {data_received[:80]} ..."),
+
+        # open file for the day and get ready to write to it
+        dte_today = datetime.now().strftime('%Y%m%d')
+        file_path = os.path.join(data_path, f"AE31_{dte_today}.csv")
+        if os.path.exists(file_path):
+            mode = 'a'
         else:
-            file_data.close()
-            dte_today = datetime.now().strftime('%Y%m%d')
-            file_data = open(f"data/ae31/AE31_{dte_today}.csv", 'w')
+            mode = 'w'
             print(f"# Reading data and writing to AE31_{dte_today}.csv")
-            file_data.write(f"{dtm}, {data_received}")
+        
+        with open(file=file_path, mode=mode) as fh:
+            fh.write(f"{dtm}, {data_received}")
+            fh.close()
+
+        ser.close()
+
+if __name__ == "__main__":
+    serial_port = '/dev/ttyUSB0'
+    data_path = os.path.join('data', 'ae31')
+
+    schedule.every(5).minutes.do(retrieve_and_save_data, serial_port, data_path)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
