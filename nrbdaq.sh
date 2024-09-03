fbb678cf10eb90333bd756f0ccd75cd3ef457ea1
jkl <joerg.klausen@meteoswiss.ch>
2024-09-03 15:18:16 +0000

    feat(nrbdaq.sh): check if script is already running

--- nrbdaq.sh
+++ nrbdaq.sh
@@ -4,14 +4,17 @@
 export PATH="/usr/local/bin:/usr/bin:/bin"
 export PYTHONPATH="/usr/lib/python311.zip:/usr/lib/python3.11:/usr/lib/python3.11/lib-dynload:/usr/local/lib/python3.11/dist-packages:/usr/lib/python3/dist-packages:/usr/lib/python3.11/dist-packages"
 
-# load virtual environment
-source .venv/bin/activate
-
-# navigate to launch folder
-cd /home/gaw/git/nrbdaq/
-
-# execute script
-python3 nrbdaq.py
+# Check if the script is already running
+if pgrep -f "python3 /home/gaw/git/nrbdaq/nrbdaq.py" > /dev/null
+then
+    echo "nrbdaq.py already running."
+    exit 1
+else
+    # load venv
+    source .venv/bin/activate
+    # Start the script
+    python python3 /home/gaw/git/nrbdaq/nrbdaq.py
+fi
 
 # NB: make sure, these are executable
 # chmod +x /home/gaw/git/nrbdaq/nrbdaq.sh
