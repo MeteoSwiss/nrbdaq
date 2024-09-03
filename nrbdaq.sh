ea577a0fbda57ebe5ebf03ac85ea16b7246564ae
jkl <joerg.klausen@meteoswiss.ch>
2024-09-03 06:27:47 +0000

    fix(cron,nrbdaq.sh): improved

--- nrbdaq.sh
+++ nrbdaq.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cd /
+
+# Set necessary environment variables
+export PATH="/usr/local/bin:/usr/bin:/bin"
+export PYTHONPATH="/usr/lib/python311.zip:/usr/lib/python3.11:/usr/lib/python3.11/lib-dynload:/usr/local/lib/python3.11/dist-packages:/usr/lib/python3/dist-packages:/usr/lib/python3.11/dist-packages"
 
 # load virtual environment
 source .venv/bin/activate
@@ -10,8 +13,6 @@ cd /home/gaw/git/nrbdaq/
 # execute script
 python3 nrbdaq.py
 
-# go back to root
-cd /
-
-# NB: make sure, this script is executable
-# chmod 755 nrbdaq.sh
+# NB: make sure, these are executable
+# chmod +x /home/gaw/git/nrbdaq/nrbdaq.sh
+# chmod +x .venv/bin/activate
