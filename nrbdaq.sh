4b4044b6e0c5485be4ece6191d9ce0f62382de67
jkl <joerg.klausen@meteoswiss.ch>
2024-09-04 13:01:09 +0000

    fix/nrbdaq.sh): path

--- nrbdaq.sh
+++ nrbdaq.sh
@@ -5,7 +5,7 @@ export PATH="/usr/local/bin:/usr/bin:/bin"
 export PYTHONPATH="/usr/lib/python311.zip:/usr/lib/python3.11:/usr/lib/python3.11/lib-dynload:/usr/local/lib/python3.11/dist-packages:/usr/lib/python3/dist-packages:/usr/lib/python3.11/dist-packages"
 
 # Check if the script is already running
-if pgrep -f "python3 /home/gaw/git/nrbdaq/nrbdaq.py" > /dev/null
+if pgrep -f "/usr/bin/python3 /home/gaw/git/nrbdaq/nrbdaq.py" > /dev/null
 then
     echo "nrbdaq.py already running."
     exit 1
@@ -17,7 +17,7 @@ else
     cd /home/gaw/git/nrbdaq/
     
     # Start the script
-    python3 nrbdaq.py
+    /usr/bin/python3 nrbdaq.py
 fi
 
 # NB: make sure, these are executable
