ed59a5e5e0d64bce45e5ac4e178f8db3bae4e228
Jörg Klausen <joerg.klausen@meteoswiss.ch>
2024-09-28 15:21:11 +0000

    feat(mqtt_client.py)

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,11 +1,20 @@
 import paho.mqtt.client as mqtt
 
-def on_message(client, userdata, message):
-    print(f"Received log: {message.payload.decode()}")
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("logs")
 
-client = mqtt.Client()
+
+def on_message(client, userdata, message):
+    print(f"{message.payload.decode()}")
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.on_connect = on_connect
 client.on_message = on_message
 client.connect("localhost", 1883, 60)
-client.subscribe("logs")
+# client.subscribe("logs")
 
 client.loop_forever()
