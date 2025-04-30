30c75e6a8c7218a6322e1128b71d94e730f391cb
Jörg Klausen <joerg.klausen@meteoswiss.ch>
2025-04-30 12:43:23 +0200

    fix:paths in bomet.sh, .yml

--- bomet.sh
+++ bomet.sh
@@ -2,7 +2,7 @@
 
 # Set necessary environment variables
 export PATH="/usr/local/bin:/usr/bin:/bin"
-export PYTHONPATH="/usr/lib/python311.zip:/usr/lib/python3.11:/usr/lib/python3.11/lib-dynload:/usr/local/lib/python3.11/dist-packages:/usr/lib/python3/dist-packages:/usr/lib/python3.11/dist-packages"
+export PYTHONPATH="/usr/lib/python313.zip:/usr/lib/python3.13:/usr/lib/python3.13/lib-dynload:/usr/local/lib/python3.13/dist-packages:/usr/lib/python3/dist-packages:/usr/lib/python3.13/dist-packages"
 
 # Ensure the named pipe exists or create it
 #PIPE="/tmp/bomet"
@@ -17,18 +17,18 @@ then
     # exit 1
 else
     # load venv
-    source /home/gaw/.venv/bin/activate
+    source /home/admin/Documents/git/.venv/bin/activate
     echo "$(date +\%FT\%T), INFO, bomet.sh, .venv activated"
 
     # change cwd
-    cd /home/gaw/git/bomet/
+    cd /home/admin/Documents/git/bomet/
 
     # Start the script
-    /home/gaw/.venv/bin/python3 -u /home/gaw/git/bomet/bomet.py
-#    /home/gaw/.venv/bin/python3 -u /home/gaw/git/bomet/bomet.py > "$PIPE"
-    echo "$(date +\%FT\%T), INFO, bomet.sh, == bomet (re)started ====" >> /home/gaw/Documents/bomet/bomet.log 2>&1
+    /home/admin/Documents/git/.venv/bin/python3 -u /home/admin/Documents/git/bomet/bomet.py
+#    /home/admin/.venv/bin/python3 -u /home/admin/git/bomet/bomet.py > "$PIPE"
+    echo "$(date +\%FT\%T), INFO, bomet.sh, == BOMET daQ (re)started ====" >> /home/admin/Documents/bomet/bomet.log 2>&1
 fi
 
 # NB: make sure, these are executable
-# chmod +x /home/gaw/git/bomet/bomet.sh
+# chmod +x /home/admin/Documents/git/bomet/bomet.sh
 # chmod +x .venv/bin/activate
