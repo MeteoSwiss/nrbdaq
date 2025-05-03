154ea41405cf65ff47d680a33768debcfa6b05e2
Jörg Klausen <joerg.klausen@meteoswiss.ch>
2025-05-03 12:11:09 +0200

    feat(nrbdaq.py,fidas.py,nrbdaq.yml):add fidas

--- nrbdaq.py
+++ nrbdaq.py
@@ -5,6 +5,7 @@ from nrbdaq.instr.ae31 import AE31
 import nrbdaq.instr.avo as avo
 from nrbdaq.instr.thermo import Thermo49i
 from nrbdaq.instr.aurora3000 import Aurora3000
+from nrbdaq.instr.fidas import FIDAS
 from nrbdaq.utils.sftp import SFTPClient
 from nrbdaq.utils.utils import load_config, setup_logging, seconds_to_next_n_minutes
 
@@ -16,11 +17,15 @@ def main():
     # setup logging
     logfile = os.path.join(os.path.expanduser(config['root']), config['logging']['file'])
     logger = setup_logging(file=logfile)
-    logger.info("== Start NRBDAQ =============")
+    logger.info("== Start NRBDAQ =============", extra={'to_logfile': True})
 
     # setup sftp client
     sftp = SFTPClient(config=config)
     logger.debug(f"sftp.remote_path: {sftp.remote_path}")
+
+    # setup FIDAS
+    fidas = FIDAS(config=config)
+    fidas.setup_schedules()
 
     # setup AE31 data acquisition and data transfer
     ae31 = AE31(config=config)
@@ -28,7 +33,7 @@ def main():
     remote_path = os.path.join(sftp.remote_path, ae31.remote_path)
     sftp.setup_transfer_schedules(local_path=ae31.staging_path,
                                   remote_path=remote_path,
-                                  interval=ae31.reporting_interval)  
+                                  interval=ae31.reporting_interval)
 
     # setup Nairobi AVO data download, staging and transfer
     data_path = os.path.join(os.path.expanduser(config['root']), config['data'], config['AVO']['data_path'])
@@ -38,12 +43,12 @@ def main():
     hours = [f"{download_interval*n:02}:00" for n in range(23) if download_interval*n <= 23]
     for hr in hours:
         schedule.every(1).day.at(hr).do(avo.download_multiple,
-                                       urls={'url_nairobi': config['AVO']['urls']['url_nairobi']}, 
-                                       file_path=data_path, 
+                                       urls={'url_nairobi': config['AVO']['urls']['url_nairobi']},
+                                       file_path=data_path,
                                        staging=staging_path)
     sftp.setup_transfer_schedules(local_path=staging_path,
                                   remote_path=remote_path,
-                                  interval=download_interval)  
+                                  interval=download_interval)
 
     # setup Thermo 49i data acquisition and data transfer
     thermo49i = Thermo49i(config=config)
@@ -51,7 +56,7 @@ def main():
     remote_path = os.path.join(sftp.remote_path, thermo49i.remote_path)
     sftp.setup_transfer_schedules(local_path=thermo49i.staging_path,
                                   remote_path=remote_path,
-                                  interval=thermo49i.reporting_interval)  
+                                  interval=thermo49i.reporting_interval)
 
     # setup Aurora3000
     neph = Aurora3000(config=config)
@@ -60,24 +65,28 @@ def main():
     remote_path = os.path.join(sftp.remote_path, neph.remote_path)
     sftp.setup_transfer_schedules(local_path=neph.staging_path,
                                   remote_path=remote_path,
-                                  interval=neph.reporting_interval)  
+                                  interval=neph.reporting_interval)
 
     # list all jobs
-    logger.info(schedule.get_jobs())
+    logger.info(schedule.get_jobs(), extra={'to_logfile': True})
 
     # align start with a multiple-of-minute timestamp
     seconds_left = seconds_to_next_n_minutes(1)
     while seconds_left > 0:
         print(f"Time remaining: {seconds_left:0.1f} s", end="\r")
-        dt = 0.2
+        dt = 1 #0.2
         time.sleep(dt)
         seconds_left -= dt
     logger.info("Beginning data acquisition and file transfer ...")
 
     # start jobs
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    try:
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Stopping data acquisition ...")
+        fidas.save_hourly()  # Save any remaining data on exit
 
 
 if __name__ == "__main__":
