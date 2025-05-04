c41c459071406c41ea00aa30d7f4037d274d5026
jkl <joerg.klausen@meteoswiss.ch>
2025-05-04 08:58:53 +0000

    feat(bucdaq):new setup for Bomet University College (BUC)

---
+++ bucdaq.py
@@ -0,0 +1,53 @@
+import os
+import schedule
+import time
+from nrbdaq.instr.thermo import Thermo49i
+from nrbdaq.utils.sftp import SFTPClient
+from nrbdaq.utils.utils import load_config, setup_logging, seconds_to_next_n_minutes
+
+
+def main():
+    # load configuation
+    config = load_config(config_file='bucdaq.yml')
+
+    # setup logging
+    logfile = os.path.join(os.path.expanduser(config['root']), config['logging']['file'])
+    logger = setup_logging(file=logfile)
+    logger.info("== Start BUCDAQ =============", extra={'to_logfile': True})
+
+    # setup sftp client
+    sftp = SFTPClient(config=config)
+    logger.debug(f"sftp.remote_path: {sftp.remote_path}")
+
+    # setup Thermo 49i data acquisition and data transfer
+    thermo49i = Thermo49i(config=config)
+    thermo49i.setup_schedules()
+    remote_path = os.path.join(sftp.remote_path, thermo49i.remote_path)
+    sftp.setup_transfer_schedules(local_path=thermo49i.staging_path,
+                                  remote_path=remote_path,
+                                  interval=thermo49i.reporting_interval)
+
+    # list all jobs
+    logger.info(schedule.get_jobs(), extra={'to_logfile': True})
+
+    # align start with a multiple-of-minute timestamp
+    seconds_left = seconds_to_next_n_minutes(1)
+    while seconds_left > 0:
+        print(f"Time remaining: {seconds_left:0.1f} s", end="\r")
+        dt = 1 #0.2
+        time.sleep(dt)
+        seconds_left -= dt
+    logger.info("Beginning data acquisition and file transfer ...")
+
+    # start jobs
+    try:
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Stopping data acquisition ...")
+        # fidas.save_hourly()  # Save any remaining data on exit
+
+
+if __name__ == "__main__":
+    main()
---
+++ bucdaq.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Set necessary environment variables
+export PATH="/usr/local/bin:/usr/bin:/bin"
+export PYTHONPATH="/usr/lib/python312.zip:/usr/lib/python3.12:/usr/lib/python3.12/lib-dynload:/usr/local/lib/python3.12/dist-packages:/usr/lib/python3/dist-packages:/usr/lib/python3.12/dist-packages"
+
+# Ensure the named pipe exists or create it
+#PIPE="/tmp/bucdaq"
+#if [ ! -p "$PIPE" ]; then
+#    mkfifo "$PIPE"
+#fi
+
+# Check if the script is already running
+if pgrep -f -a "bucdaq.py" > /dev/null
+then
+    echo "$(date +\%FT\%T), INFO, bucdaq.sh, bucdaq.py already running."
+    # exit 1
+else
+    # load venv
+    source /home/admin/git/nrbdaq/.venv/bin/activate
+    echo "$(date +\%FT\%T), INFO, bucdaq.sh, .venv activated"
+
+    # change cwd
+    cd /home/admin/git/bucdaq/
+
+    # Start the script
+    /home/admin/git/nrbdaq/.venv/bin/python3 -u /home/admin/git/bucdaq/bucdaq.py
+    echo "$(date +\%FT\%T), INFO, bucdaq.sh, == BUCDAQ (re)started ====" >> /home/admin/Documents/bucdaq/bucdaq.log 2>&1
+fi
+
+# NB: make sure, these are executable
+# chmod +x /home/admin/git/bucdaq/bucdaq.sh
+# chmod +x .venv/bin/activate
