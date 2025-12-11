9a5d8324132de2450b08855bb2d3b2e6aba1a269
Jörg Klausen <joerg.klausen@meteoswiss.ch>
2025-12-11 18:39:25 +0300

    fix(hmp110_scan_ascii.py):correct ttyUSB

--- hmp110_ascii_scan.py
+++ hmp110_ascii_scan.py
@@ -26,7 +26,7 @@ import serial
 
 
 # You can override this via --port on the CLI
-PORT_DEFAULT = "/dev/ttyUSB8"
+PORT_DEFAULT = "/dev/ttyUSB2"
 
 # Candidate (baudrate, parity, stopbits, bytesize) combinations.
 # Based on HMP60/110 manual defaults and allowed SERI settings.
